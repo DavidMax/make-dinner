3d762b08c7bb6b9b4129cbb6a082182dad8f8550
David Max <davidmax@Davids-MacBook-Pro.local>
2017-06-02 16:42:49 -0700

    Create recipe-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from "./header/header.component";
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
